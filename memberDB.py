b4da98a7d4db6e6ab6831c364b03c98a2619b8f2
gyojincompany <luckyred@gmail.com>
2024-01-30 18:58:15 +0900

    db 접속 연습텍스트 기반 프로그램

--- memberDB.py
+++ memberDB.py
@@ -17,6 +17,13 @@ if menu == '1':
     memberage = input("4) 회원나이 : ")
 
     sql = f"INSERT INTO pymember VALUES('{memberid}','{memberpw}','{membername}','{memberage}')"
+    cur = dbConn.cursor()
+    result = cur.execute(sql)  # sql문이 지정된 데이터베이스에서 실행됨->sql문이 성공하면 1이 반환
+
+    if (result == 1):
+        print("데이터베이스가 실행한 명령을 성공하였습니다!")
+    else:
+        print("데이터베이스가 실행한 명령을 실패하였습니다!")
 
 elif menu == '2':
     print("------ 회원비밀번호 수정 ------")
@@ -25,19 +32,43 @@ elif menu == '2':
 
     sql = f"UPDATE pymember SET memberpw='{memberpw}' WHERE memberid='{memberid}'"
 
+    cur = dbConn.cursor()
+    result = cur.execute(sql)
+
+    if (result == 1):
+        print("데이터베이스가 실행한 명령을 성공하였습니다!")
+    else:
+        print("데이터베이스가 실행한 명령을 실패하였습니다!")
+
+
 elif menu == '3':
     print("------ 회원 삭제 ------")
     memberid = input("탈퇴할 회원의 아이디를 입력하세요 : ")
 
     sql = f"DELETE FROM pymember WHERE memberid='{memberid}'"
 
+    cur = dbConn.cursor()
+    result = cur.execute(sql)
+
+    if (result == 1):
+        print("데이터베이스가 실행한 명령을 성공하였습니다!")
+    else:
+        print("데이터베이스가 실행한 명령을 실패하였습니다!")
+
+elif menu == '4':
+    print("------ 회원 리스트 ------")
+    sql = "SELECT * FROM pymember"
+
+    cur = dbConn.cursor()  # sql문을 실행시켜줄 커넥션내의 함수 호출
+    cur.execute(sql)  # sql문이 지정된 데이터베이스에서 실행됨->sql문이 성공하면 1이 반환
+    rows = cur.fetchall()  # select문의 결과를 튜플구조로 반환
+
+    for member in rows:
+        print(member)
 else:
     print("***** 잘못된 번호입니다. 프로그램을 종료합니다. *****")
 
-# 위에서 만들어진 sql문을 실행하는 구문
 
-cur = dbConn.cursor()  # sql문을 실행시켜줄 커넥션내의 함수 호출
-cur.execute(sql)  # sql문이 지정된 데이터베이스에서 실행됨
 
 cur.close()
 dbConn.commit()
