449b34a995bcab401b27bebcaff1ca29ac83b742
gyojincompany <luckyred@gmail.com>
2024-01-30 21:14:51 +0900

    memberApp 회원 id 중복여부 체크 기능 추가

--- memberApp.py
+++ memberApp.py
@@ -17,6 +17,8 @@ class MainWindow(QMainWindow, form_class):
 
         self.join_btn.clicked.connect(self.memberJoin)  # 회원가입 버튼이 클릭되면 memberJoin 함수 호출
         self.joinreset_btn.clicked.connect(self.joinReset)  # 회원가입탭에서 초기화 클릭시 모든 입력사항 삭제
+        self.idcheck_btn.clicked.connect(self.idCheck)
+        # 현재 아이디 입력창에 입력된 아이디를 가져와 db에 존재하는지 여부 체크
 
     def memberJoin(self):  # 회원 가입 함수
         memberid = self.joinid_edit.text()  # 유저가 입력한 회원아이디 텍스트 가져오기
@@ -47,6 +49,24 @@ class MainWindow(QMainWindow, form_class):
         self.joinname_edit.clear()
         self.joinemail_edit.clear()
         self.joinage_edit.clear()
+
+    def idCheck(self):
+        memberid = self.joinid_edit.text()
+        dbConn = pymysql.connect(host='localhost', user='root', password='12345', db='pydb')
+        sql = f"SELECT * FROM appmember WHERE memberid='{memberid}'"
+
+        cur = dbConn.cursor()
+        cur.execute(sql)
+
+        result = cur.fetchone()
+
+        if result != None:  # 참이면 현재 조회하려는 아이디가 DB에 이미 존재함
+            QMessageBox.warning(self, "회원 가입 불가", "입력하신 아이디는 이미 존재하는 아이디입니다. 다른 아이디를 입력하세요.")
+        else:
+            QMessageBox.warning(self, "회원 가입 가능", "입력하신 아이디는 가입가능한 아이디입니다. 계속해서 가입 진행하세요.")
+
+        cur.close()
+        dbConn.close()
 
 
 app = QApplication(sys.argv)
